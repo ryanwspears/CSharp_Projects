5f7fd60536749ddbc4824355923c9e1ad087cb46
ryanwspears <spearswesley5@gmail.com>
2021-11-06 16:56:44 -0500

    Created Acme Accounting project.

---
+++ AcmeAccounting/Acme_Accounting/Acme_Accounting.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Acme_Accounting.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
