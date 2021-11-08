de05df12d571d4e93424e3c26c4dfa920a26a93d
ryanwspears <spearswesley5@gmail.com>
2021-11-08 12:39:26 -0600

    Created doWhile.cs

---
+++ doWhile/doWhile/doWhile.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace doWhile.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Guess the anime character\n\nType your guess...");
+            string guess = Console.ReadLine();
+
+            bool guessed = guess == "Saitama";
+
+            do
+            {
+                switch(guess) {
+                    case "Saitama":
+                        Console.WriteLine("\n\nYou are correct!");
+                        guessed = true;
+                        Console.Read();
+                        break;
+                    default:
+                        Console.WriteLine("\n\nThat is incorrect. Try again.\n");
+                        Console.WriteLine("Type your guess...");
+                        guess = Console.ReadLine();
+                        break;
+                }
+            }
+            while(!guessed);
+        }
+    }
+}
