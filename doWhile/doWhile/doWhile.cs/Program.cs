2d78eede417af4642dfc0067d395383232a48e7b
ryanwspears <spearswesley5@gmail.com>
2021-11-08 13:57:59 -0600

    Edited doWhile.cs

--- doWhile/doWhile/doWhile.cs/Program.cs
+++ doWhile/doWhile/doWhile.cs/Program.cs
@@ -10,23 +10,40 @@ namespace doWhile.cs
             string guess = Console.ReadLine();
 
             bool guessed = guess == "Saitama";
+            int tries = 5;
 
-            do
+            while(tries > 0)
             {
-                switch(guess) {
-                    case "Saitama":
-                        Console.WriteLine("\n\nYou are correct!");
-                        guessed = true;
-                        Console.Read();
-                        break;
-                    default:
-                        Console.WriteLine("\n\nThat is incorrect. Try again.\n");
-                        Console.WriteLine("Type your guess...");
-                        guess = Console.ReadLine();
-                        break;
+                do
+                {
+                    switch (guess)
+                    {
+                        case "Saitama":
+                            Console.WriteLine("\n\nYou are correct! You win!");
+                            guessed = true;
+                            Console.Read();
+                            return;
+                            break;
+                        default:
+                            tries--;
+
+                            if (tries == 0)
+                            {
+                                Console.WriteLine("\n\nYou are out of tries. You lose...");
+                                Console.Read();
+                                return;
+                            }
+
+                            Console.WriteLine("\n\nThat is incorrect. Try again. Tries left: " + tries + "\n");
+                            Console.WriteLine("Type your guess...");
+                            guess = Console.ReadLine();
+                            break;
+                    }
+
                 }
+                while (!guessed);
             }
-            while(!guessed);
+
         }
     }
 }
