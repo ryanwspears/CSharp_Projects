9dd7b9f97e3f62bc0dc91f78cb6754a75a9b1e07
ryanwspears <spearswesley5@gmail.com>
2021-11-08 22:06:41 -0600

    Created tryCatch.cs

---
+++ tryCatch/tryCatch/tryCatch.cs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tryCatch.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>
+            {
+                2,
+                4,
+                6,
+                8,
+                10,
+                3,
+                5,
+                7,
+                9
+            };
+
+            try
+            {
+                Console.WriteLine("Enter a number to divide list integers by...");
+                int num = Convert.ToInt32(Console.ReadLine());
+                foreach (int i in intList)
+                {
+                    int dividend = i / num;
+                    Console.WriteLine(i + " divided by " + num + " equals " + dividend);
+                }
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Program has completed successfully.");
+            Console.Read();
+        }
+    }
+}
