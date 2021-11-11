8ca91cc4462315b1dd78d62c9df59d865f1d626e
ryanwspears <spearswesley5@gmail.com>
2021-11-10 21:21:16 -0600

    Created MethodClass.cs

---
+++ MethodClass/MethodClass/MethodClass.cs/Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass.cs
+{
+    class Method
+    {
+        public void Operation(int num1, int num2)
+        {
+            int op = num1 * 5;
+
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/MethodClass.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method method = new Method();
+
+            method.Operation(num1: 4, num2: 6);
+
+            Console.Read();
+        }
+    }
+}
