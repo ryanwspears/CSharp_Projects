ddcf67e746b188b3d4588a5d9aaf759b6559f339
ryanwspears <spearswesley5@gmail.com>
2021-11-12 12:41:31 -0600

    Created DateTime.cs

---
+++ DateTime/DateTime/DateTime.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace DateTime.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For some reason, I can't use DateTime.Now
+            // This is the only thing that worked for me. I even restarted my laptop.
+            DateTimeOffset date = DateTimeOffset.Now;
+
+            Console.WriteLine(date);
+
+            Console.WriteLine("Please enter a number...");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            date = date.AddHours(num);
+
+            Console.WriteLine("The time in " + num + " hours will be " + date);
+
+            Console.Read();
+        }
+    }
+}
