e8d4f43de5c6e8a0804b86061eeab67b8b25fdc9
ryanwspears <spearswesley5@gmail.com>
2021-11-12 15:09:34 -0600

    Created Constructor.cs

---
+++ Constructor/Constructor/Constructor.cs/Construct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.cs
+{
+    public class Construct
+    {
+        public string name;
+        public int id;
+
+        public Construct() : this("Ryan", 77)
+        {
+            
+        }
+        public Construct(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public Construct(string name) : this(name, 93)
+        {
+
+        }
+    }
+}
---
+++ Constructor/Constructor/Constructor.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const variable
+            const string name = "Ryan";
+
+            // var variable
+            var id = 77;
+
+            // Chain 3 constructors together
+            Construct con1 = new Construct(), con2 = new Construct("Robb", 83), con3 = new Construct("Dylan");
+
+            Console.WriteLine(con1.name + " " + con1.id);
+            Console.WriteLine(con2.name + " " + con2.id);
+            Console.WriteLine(con3.name + " " + con3.id);
+
+            Console.Read();
+        }
+    }
+}
