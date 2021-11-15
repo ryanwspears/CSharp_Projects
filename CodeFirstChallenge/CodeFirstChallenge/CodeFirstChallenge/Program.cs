519f135c51b99bbdda0c055042244444d9fc865b
ryanwspears <spearswesley5@gmail.com>
2021-11-15 17:02:23 -0600

    Created CodeFirstChallenge

---
+++ CodeFirstChallenge/CodeFirstChallenge/CodeFirstChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var studentCtx = new StudentContext())
+            {
+                var student = new Student() { StudentId = 0, StudentFName = "Ryan", StudentLName = "Spears", StudentGrade = 12 };
+
+                studentCtx.students.Add(student);
+                studentCtx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CodeFirstChallenge/CodeFirstChallenge/CodeFirstChallenge/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentFName { get; set; }
+        public string StudentLName { get; set; }
+        public int StudentGrade { get; set; }
+    }
+}
